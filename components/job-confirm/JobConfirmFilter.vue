2e10f8eddc44c2a1173a6d303325b2610781e6f2
phucnh17 <phucnh17@msb.com.vn>
2024-06-06 03:02:14 +0700

    feat: clone job confirm

--- components/job-confirm/JobConfirmFilter.vue
+++ components/job-confirm/JobConfirmFilter.vue
@@ -17,7 +17,8 @@
       </el-col>
 
       <el-col :span="3">
-        <el-select v-model="jobConfirmStore.filter.status" placeholder="Trạng thái">
+        <el-select v-model="jobConfirmStore.filter.status" placeholder="Trạng thái"
+          @change="jobConfirmStore.fetchJobConfirm">
           <el-option v-for="item in jobConfirmStore.data.optionsStatus" :key="item.value" :label="item.label"
             :value="item.value" />
         </el-select>
