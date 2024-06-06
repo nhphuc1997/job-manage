dcb045d055d367d927a6578320a434a1d2858b1e
phucnh17 <phucnh17@msb.com.vn>
2024-06-06 16:51:22 +0700

    feat: clone auth

--- components/resume/ResumeDataTable.vue
+++ components/resume/ResumeDataTable.vue
@@ -6,12 +6,31 @@
       <el-table-column prop="userName" label="Tên người dùng" sortable />
       <el-table-column prop="fullName" label="Tên người dùng" sortable />
       <el-table-column prop="phoneNumber" label="Số điện thoại" sortable />
-      <el-table-column prop="profileUrl" label="Url hồ sơ" sortable width="400" />
-      <el-table-column prop="status" label="Trạng thái" sortable width="200" />
+      <el-table-column prop="profileUrl" label="Url hồ sơ" align="center">
+        <template #default="scope">
+          <el-button size="small" type="primary" round plain :icon="TopRight" @click="redirectTo(scope.row.profileUrl)">
+            Link
+          </el-button>
+        </template>
+      </el-table-column>
+      <el-table-column prop="status" label="Trạng thái" sortable width="200" align="center">
+        <template #default="scope">
+          <el-text>
+            <el-button plain size="small" :type="scope.row.status === 'FULL_UPDATED' ? 'success' : 'warning'" round>
+              {{ String(scope.row.status).toLowerCase() }}
+            </el-button>
+          </el-text>
+        </template>
+      </el-table-column>
     </el-table>
   </div>
 </template>
 
 <script lang="ts" setup>
+import { TopRight } from '@element-plus/icons-vue';
 const resumeStore = useResumeStore()
+
+const redirectTo = (path: string) => {
+  return navigateTo({ path }, { open: { target: '_blank' } })
+}
 </script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,5 +10,6 @@ export default defineNuxtConfig({
   css: [`~/assets/styles/main.scss`],
   devServer: {
     port: 9000
-  }
+  },
+  ssr: false,
 })
