eb0fe4b8492a1070ce6d39daaf08e4432e44bb48
nhphuc1997 <nguyen.hong.phuc20211997@gmail.com>
2024-06-13 02:04:34 +0700

    feat: ignore pem file

--- components/area/AreaFilter.vue
+++ components/area/AreaFilter.vue
@@ -7,19 +7,19 @@
           placeholder="Tìm kiếm tên, mã khu vực" :suffix-icon="ElIconSearch" />
       </el-col>
 
-      <el-col :span="4">
+      <el-col :span="2">
         <el-select v-model="areaStore.filter.status" placeholder="Trạng thái" @change="areaStore.fetchAreas">
           <el-option v-for="item in areaStore.data.listStatus" :key="item.value" :label="item.label"
             :value="item.value" />
         </el-select>
       </el-col>
 
-      <el-col :span="4">
+      <el-col :span="2" :offset="12">
         <el-button @click="areaStore.resetFilter" plain style="width: 100%;" type="info" :icon="ElIconMug">Xoá bộ
           lọc</el-button>
       </el-col>
 
-      <el-col :span="4" :offset="6">
+      <el-col :span="2">
         <el-button @click="areaStore.dialog.createAreaVisible = true" plain style="width: 100%;" type="success"
           :icon="ElIconWindPower">Tạo khu vực</el-button>
       </el-col>
