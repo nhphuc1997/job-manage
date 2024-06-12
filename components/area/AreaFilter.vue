3e9e0de5f2586374a82530938ce4230c35132dc8
phucnh17 <phucnh17@msb.com.vn>
2024-06-12 13:41:24 +0700

    feat: clone auth

--- components/area/AreaFilter.vue
+++ components/area/AreaFilter.vue
@@ -1,24 +1,24 @@
 <template>
   <div class="create">
-    <el-row :gutter="12">
-      <el-col :span="6">
+    <el-row :gutter="4">
+      <el-col :xs="24" :sm="24" :md="6" :lg="6" class="px-4">
         <el-input v-model="areaStore.filter.fulltext" @change="areaStore.fetchAreas"
           placeholder="Tìm kiếm tên, mã khu vực" :suffix-icon="ElIconSearch" />
       </el-col>
 
-      <el-col :span="4">
+      <el-col :xs="24" :sm="8" :md="3" :lg="3" class="px-4">
         <el-select v-model="areaStore.filter.status" placeholder="Trạng thái" @change="areaStore.fetchAreas">
           <el-option v-for="item in areaStore.data.listStatus" :key="item.value" :label="item.label"
             :value="item.value" />
         </el-select>
       </el-col>
 
-      <el-col :span="3">
+      <el-col :xs="12" :sm="8" :md="3" :lg="3" class="px-4">
         <el-button @click="areaStore.resetFilter" plain style="width: 100%;" type="info" :icon="ElIconMug">Xoá bộ
           lọc</el-button>
       </el-col>
 
-      <el-col :span="3" :offset="8">
+      <el-col :xs="12" :sm="8" :md="3" :lg="3" class="px-4">
         <el-button @click="areaStore.dialog.createAreaVisible = true" plain style="width: 100%;" type="success"
           :icon="ElIconWindPower">Tạo khu vực</el-button>
       </el-col>
