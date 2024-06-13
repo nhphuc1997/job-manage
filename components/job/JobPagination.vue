fa4821b1d029d371182f34cdb090e0e681c4a859
phucnh17 <phucnh17@msb.com.vn>
2024-06-13 13:51:29 +0700

    feat: clone auth

--- components/job/JobPagination.vue
+++ components/job/JobPagination.vue
@@ -2,11 +2,15 @@
   <div class="footer">
     <el-row>
       <el-col :span="24">
-        <div class="paginate">
+        <div class="paginate hidden-sm-and-down">
           <el-pagination v-model:current-page="jobStore.metadata.currentPage" v-model:page-size="jobStore.metadata.size"
             :page-sizes="[5, 10, 100]" layout="total, sizes, prev, pager, next, jumper"
             :total="jobStore.metadata.totalElements" @size-change="jobStore.paginationSizeChange"
             @current-change="jobStore.paginationPageChange" />
+        </div>
+
+        <div class="paginate hidden-md-and-up">
+          <el-pagination layout="prev, pager, next" :total="jobStore.metadata.totalElements" />
         </div>
       </el-col>
     </el-row>
@@ -15,6 +19,7 @@
 
 <style lang="scss" scoped>
 @import url('/assets/styles/job.scss');
+@import url('element-plus/theme-chalk/display.css');
 </style>
 
 <script lang="ts" setup>
