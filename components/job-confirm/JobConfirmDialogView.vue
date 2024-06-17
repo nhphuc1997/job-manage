1f809d1ad13ad1e3fa71e400347dd327c9905fde
phucnh17 <phucnh17@msb.com.vn>
2024-06-17 10:01:40 +0700

    feat: clone auth

--- components/job-confirm/JobConfirmDialogView.vue
+++ components/job-confirm/JobConfirmDialogView.vue
@@ -78,30 +78,34 @@
           <el-descriptions-item label="Số điện thoại:">
             <el-tag>{{ jobConfirmStore.data.viewResume.phoneNumber }}</el-tag>
           </el-descriptions-item>
+
+          <el-descriptions-item label="Trạng thái:">
+            <el-tag :type="parseElTagStatus(jobConfirmStore.data.viewResume.status)">
+              {{ parseStatus(jobConfirmStore.data.viewResume.status) }}
+            </el-tag>
+          </el-descriptions-item>
         </el-descriptions>
 
-        <div>
-          <el-collapse accordion>
-            <el-collapse-item :name="item.key" v-for="item in KEYS_RESUME ">
-              <template #title>
-                <el-icon size="16">
-                  <ElIconPlatform />
-                </el-icon>
-                <el-badge type="primary" :value="jobConfirmStore.data.certs[item.key].length" :offset="[10, 15]">
-                  <el-text class="text-intro">{{ item.name }}</el-text>
-                </el-badge>
-              </template>
-              <div class="flex">
-                <el-row :gutter="10">
-                  <el-col :span="4" v-for="photo in jobConfirmStore.data.certs[item.key]">
-                    <el-image class="image" :src="photo" :zoom-rate="1.2" :max-scale="7" :min-scale="0.2"
-                      :initial-index="4" fit="cover" :preview-src-list="[photo]" />
-                  </el-col>
-                </el-row>
-              </div>
-            </el-collapse-item>
-          </el-collapse>
-        </div>
+        <el-collapse accordion>
+          <el-collapse-item :name="item.key" v-for="item in KEYS_RESUME ">
+            <template #title>
+              <el-icon size="16">
+                <ElIconPlatform />
+              </el-icon>
+              <el-badge type="primary" :value="jobConfirmStore.data.certs[item.key].length" :offset="[10, 15]">
+                <el-text class="text-intro">{{ item.name }}</el-text>
+              </el-badge>
+            </template>
+            <div class="flex">
+              <el-row :gutter="10">
+                <el-col :span="4" v-for="photo in jobConfirmStore.data.certs[item.key]">
+                  <el-image class="image" :src="photo" :zoom-rate="1.2" :max-scale="7" :min-scale="0.2"
+                    :initial-index="4" fit="cover" :preview-src-list="[photo]" />
+                </el-col>
+              </el-row>
+            </div>
+          </el-collapse-item>
+        </el-collapse>
       </el-tab-pane>
 
     </el-tabs>
@@ -117,4 +121,23 @@
 <script lang="ts" setup>
 const formLabelWidth = '140px'
 const jobConfirmStore = useJobConfirmStore()
+
+const parseStatus = (type: string) => {
+  return {
+    FULL_INFO: 'Đủ thông tin',
+    NEED_ADDITIONAL_INFO: 'Thiếu thông tin bổ sung',
+    NEED_MANDATORY_INFO: 'Thiếu thông tin bắt buộc'
+  }[type]
+}
+
+const parseElTagStatus = (type: string): any => {
+  if (type) {
+    return {
+      FULL_INFO: 'success',
+      NEED_ADDITIONAL_INFO: 'warning',
+      NEED_MANDATORY_INFO: 'danger'
+    }[type]
+  }
+  return 'info'
+}
 </script>
