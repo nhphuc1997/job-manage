a544a87a7a2b12e8f52ff86d376a265fc9de6482
phucnh17 <phucnh17@msb.com.vn>
2025-01-09 20:23:21 +0700

    feat: super market admin

--- components/banner/BannerDataTable.vue
+++ components/banner/BannerDataTable.vue
@@ -7,6 +7,11 @@
           <el-image style="width: 100px; height: 100px" :src="scoped.row.url" />
         </template>
       </el-table-column>
+      <el-table-column align="center" width="70" label="Xoá">
+        <template #default="scope">
+          <el-button plain :icon="ElIconRemove" size="small" @click="bannerStore.removeEntity(scope.row)" />
+        </template>
+      </el-table-column>
     </el-table>
   </div>
 </template>
